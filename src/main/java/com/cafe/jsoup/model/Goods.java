41037d506b30a3757ad09a714c94aa4947f7b0e7
zhouboyi <1144188685@qq.com>
2022-07-22 20:44:20 +0800

    使用JSoup包爬取数据

---
+++ src/main/java/com/cafe/jsoup/model/Goods.java
@@ -0,0 +1,69 @@
+package com.cafe.jsoup.model;
+
+/**
+ * @Project: mall-jsoup
+ * @Package: com.cafe.jsoup.model
+ * @Author: zhouboyi
+ */
+public class Goods {
+
+    private String name;
+
+    private String category;
+
+    private String brand;
+
+    private Double price;
+
+    private String image;
+
+    private String caption;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+}
